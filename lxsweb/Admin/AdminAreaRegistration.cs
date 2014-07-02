4a35956643e49bfd605f260724945ccb616b3ff8
lingsu <q25a25q@live.com>
2014-07-02 21:28:19 +0800

    第一次同步

---
+++ lxsweb/Admin/AdminAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Admin
+{
+    public class AdminAreaRegistration:AreaRegistration
+    {
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            //此路由不能删除
+            context.MapRoute("Admin_default",
+                              "Admin/{controller}/{action}",
+                              new { controller = "Home", action = "Index", area = "Admin" },
+                              new[] { "Admin.Controllers" });
+        }
+
+        public override string AreaName
+        {
+            get { return "Admin"; }
+        }
+    }
+}
